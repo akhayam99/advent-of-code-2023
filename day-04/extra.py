b5405efe37c6ef0408624a2198aaf380a6e05558
Amin Khayam <aminkhayam@fattureincloud.it>
2023-12-05 00:34:11 +0100

    improve day 4 part 2

--- day-04/extra.py
+++ day-04/extra.py
@@ -4,11 +4,9 @@ sys.path.append('/Users/aminkhayam/GIT/private/advent-of-code-2023/utils')
 from utils import getLinesFromFile, getNumbers
 
 lines = getLinesFromFile("day-04")
-instances = [0] * len(lines)
+instances = [1] * len(lines)
 
 for i, line in enumerate(lines):
-    instances[i] += 1
-
     played, extracted = map(getNumbers, line.split(':')[1].split('|'))
 
     for j in range(1, sum(1 for number in played if number in extracted) + 1):

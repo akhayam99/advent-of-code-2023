3d55312165fb63dc9635505bf266f46584c41d02
Amin Khayam <aminkhayam@fattureincloud.it>
2023-12-08 15:03:00 +0100

    complete day 8 part 2 + fix files to untrack

--- day-08/extra.py
+++ day-08/extra.py
@@ -1,12 +1,35 @@
-import sys
+import sys, re, math
 sys.path.append('/Users/aminkhayam/GIT/private/advent-of-code-2023/utils')
 
 from utils import getLinesFromFile
 
-result = 0
-lines = getLinesFromFile("day-04")
+lines = getLinesFromFile("day-08")
 
+myMap = []
+myMatches = []
+paths = [char for char in lines[0].replace('\n', '')]
+
+pattern = re.compile(r'\w{3}')
 for line in lines:
-    print("do things here")
+    if ' = ' not in line:
+        continue
+    values = pattern.findall(line)
+    myMap.append({values[0]: values[1:len(values)]})
+    if(values[0].endswith('A')):
+        myMatches.append(values[0])
 
-print(result)
+succeded = []
+counter, index = 1, 0
+while len(succeded) != len(myMatches):
+    index = 0 if index >= len(paths) else index
+    for i, match in enumerate(myMatches):
+        combination = next((arr[match] for arr in myMap if match in arr), None)
+        prevMatch = myMatches[i]
+        myMatches[i] = combination[1] if paths[index] == "R" else combination[0]
+        if(myMatches[i].endswith('Z')):
+            succeded.append(counter)
+    index += 1; counter += 1
+
+print(print(math.lcm(*succeded)))
+
+# Answer: 19185263738117

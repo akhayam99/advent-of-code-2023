29ae6afb222b8df5478a44bf233ea5befa032f91
Emanuele Saccomandi <48432054+emajo@users.noreply.github.com>
2023-12-09 18:36:29 +0100

    saccomandizzazione

--- day-09/solution.py
+++ day-09/solution.py
@@ -3,21 +3,15 @@ sys.path.append('/Users/aminkhayam/GIT/private/advent-of-code-2023/utils')
 
 from utils import getLinesFromFile
 
-result = 0
-for line in getLinesFromFile("day-09"):
-    history = [[int(x) for x in line.replace('\n', '').split(' ')]]
+for line in getLinesFromFile("inputs"):
+    items = [int(x) for x in line.replace('\n', '').split(' ')]
 
-    while any(history[len(history) - 1]):
+    while any(items):
+        result += items[len(items) - 1]
         process = []
-        items = history[len(history) - 1]
         for index in range(0, len(items) - 1, 1):
             process.append(int(items[index + 1]) - int(items[index]))
-        history.append(process)
-
-    value = 0
-    for items in reversed(history):
-        value += items[len(items) - 1]
-    result += value
+        items = process
 
 print(result)
 

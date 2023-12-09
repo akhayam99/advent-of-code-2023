e8523303e0a12274c571b5f8a197a82433d47de1
Amin Khayam <aminkhayam@fattureincloud.it>
2023-12-09 06:46:56 +0100

    complete day 9 part 2

--- day-09/extra.py
+++ day-09/extra.py
@@ -4,9 +4,27 @@ sys.path.append('/Users/aminkhayam/GIT/private/advent-of-code-2023/utils')
 from utils import getLinesFromFile
 
 result = 0
-lines = getLinesFromFile("day-04")
+lines = getLinesFromFile("day-09")
 
+histories = []
 for line in lines:
-    print("do things here")
+    histories.append([int(x) for x in line.replace('\n', '').split(' ')])
+
+for history in histories:
+    process = [history]
+
+    while any(process[len(process) - 1]):
+        toAppend = []
+        items = process[len(process) - 1]
+        for index in range(0, len(items) - 1, 1):
+            toAppend.append(int(items[index + 1]) - int(items[index]))
+        process.append(toAppend)
+
+    value = 0
+    for items in reversed(process):
+        value = -value + items[0]
+    result += value
 
 print(result)
+
+# Answer: 1987402313

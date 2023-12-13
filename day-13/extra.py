7feec7a0d67ce9ad83bef09deba4fdb272d8c339
Amin Khayam <aminkhayam@fattureincloud.it>
2023-12-13 11:27:51 +0100

    Improve day-13/extra

--- day-13/extra.py
+++ day-13/extra.py
@@ -3,104 +3,52 @@ sys.path.append('/Users/aminkhayam/GIT/private/advent-of-code-2023/utils')
 
 from utils import getLinesFromFile
 
-lines = getLinesFromFile("day-13")
-
-result = 0
-reg = re.compile(r'[#\.]')
-
-blocks = [[]]
-index = 0
-
-for line in lines:
-    if not line == "\n":
-        blocks[index].append(reg.findall(line))
-        continue
-    blocks.append([])
-    index += 1
-
 def validSmudge(p1, p2):
     return sum(c1 != c2 for c1, c2 in zip(p1, p2)) == 1
 
-def checkNextPattern(patterns, index, smudges):
-    span = 1
+def processPatterns(patterns, index, smudges):
+    if index == 0 or index == len(patterns) - 2:
+        return [True, smudges + 1] if validSmudge(patterns[index], patterns[index + 1]) else [False, smudges]
 
-    if index == 0:
-        if validSmudge(patterns[index], patterns[index + 1]):
-            return [True, smudges + 1]
-        if patterns[index] == patterns[index + 1]:
-            return [True, smudges]
-
-    if index == len(patterns) - 2:
-        if validSmudge(patterns[index], patterns[index + 1]):
-            return [True, smudges + 1]
-        if patterns[index] == patterns[index + 1]:
-            return [True, smudges]
-
-    while True:
-        if index - span < 0 or index + 1 + span > len(patterns) - 1:
+    for j in range(1, 999999):
+        if index - j < 0 or index + 1 + j > len(patterns) - 1:
             return [False, smudges]
 
-        if not patterns[index - span] == patterns[index + 1 + span]:
-            if validSmudge(patterns[index - span], patterns[index + 1 + span]) and smudges == 0:
-                smudges += 1
-            else:
+        if not patterns[index - j] == patterns[index + 1 + j]:
+            if not validSmudge(patterns[index - j], patterns[index + 1 + j]) and smudges == 0:
                 return [False, smudges]
-
-        if index - span == 0 or index + 1 + span == len(patterns) - 1:
-            return [True, smudges]
-
-        span += 1
-
-def checkPatterns(patterns):
-    smudges = 0
-    for index, pattern in enumerate(patterns):
-        if index + 1 >= len(patterns) or (not pattern == patterns[index + 1] and not validSmudge(pattern, patterns[index + 1])):
-            continue
-
-        if validSmudge(pattern, patterns[index + 1]) and (not index == 0 and not index == len(patterns) - 2):
             smudges += 1
 
-        valid, counter = checkNextPattern(patterns, index, smudges)
+        if index - j == 0 or index + 1 + j == len(patterns) - 1:
+            return [True, smudges]
 
-        if valid and counter == 1:
-            return index + 1
-
+def checkBlock(list):
+    for i, el in enumerate(list):
         smudges = 0
+        if i + 1 >= len(list) or (not el == list[i + 1] and not validSmudge(el, list[i + 1])):
+            continue
+
+        smudges += 1 if validSmudge(el, list[i + 1]) and (not i == 0 and not i == len(list) - 2) else 0
+        if (fnResult := processPatterns(list, i, smudges)) and fnResult[1] == 1:
+            return i + 1
 
     return -1
 
+def getResultFromPatterns(patterns):
+    if not (position := checkBlock(patterns)) == -1:
+        return position * 100
+    if not (position := checkBlock([list(x) for x in zip(*patterns)])) == -1:
+        return position
+    return 0
 
-for blockIndex, patterns in enumerate(blocks):
-    print(f'Block number {blockIndex + 1}')
+patterns, result = [], 0
+for line in getLinesFromFile("day-13"):
+    if not line == "\n":
+        patterns.append(re.compile(r'[#\.]').findall(line)); continue
+    result, patterns = result + getResultFromPatterns(patterns), []
 
-    for pattern in patterns:
-        foundMode = None
-        foundIndex = 0
-
-        validIndex = checkPatterns(patterns)
-        if not validIndex == -1:
-            foundMode = "HORIZONTAL"
-            foundIndex = validIndex
-
-        if foundMode == None:
-            validIndex = checkPatterns([list(x) for x in zip(*patterns)])
-            if not validIndex == -1:
-                foundMode = "VERTICAL"
-                foundIndex = validIndex
-
-        if foundMode == "HORIZONTAL":
-            result += (foundIndex * 100)
-
-        if foundMode == "VERTICAL":
-            result += foundIndex
-
-        print("  Found mode", foundMode)
-        print("  Found index", foundIndex)
-
-        print("")
-        break
+result += getResultFromPatterns(patterns)
 
 print("Result", result)
 
-
-# Answer: 37864 too low
+# Answer: 44615

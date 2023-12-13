7f88925b4df15e85dff20a6ddfc3deb4b4f60ad7
Amin Khayam <aminkhayam@fattureincloud.it>
2023-12-13 11:37:49 +0100

    Improve day-13/extra

--- day-13/extra.py
+++ day-13/extra.py
@@ -3,52 +3,46 @@ sys.path.append('/Users/aminkhayam/GIT/private/advent-of-code-2023/utils')
 
 from utils import getLinesFromFile
 
-def validSmudge(p1, p2):
+def isSmudge(p1, p2):
     return sum(c1 != c2 for c1, c2 in zip(p1, p2)) == 1
 
-def processPatterns(patterns, index, smudges):
-    if index == 0 or index == len(patterns) - 2:
-        return [True, smudges + 1] if validSmudge(patterns[index], patterns[index + 1]) else [False, smudges]
+def process(l, i, s):
+    if i == 0 or i == len(l) - 2:
+        return [True, s + 1] if isSmudge(l[i], l[i + 1]) else [False, s]
 
     for j in range(1, 999999):
-        if index - j < 0 or index + 1 + j > len(patterns) - 1:
-            return [False, smudges]
+        if i - j < 0 or i + 1 + j > len(l) - 1:
+            return [False, s]
 
-        if not patterns[index - j] == patterns[index + 1 + j]:
-            if not validSmudge(patterns[index - j], patterns[index + 1 + j]) and smudges == 0:
-                return [False, smudges]
-            smudges += 1
+        if not l[i - j] == l[i + 1 + j]:
+            if not isSmudge(l[i - j], l[i + 1 + j]) and s == 0:
+                return [False, s]
+            s += 1
 
-        if index - j == 0 or index + 1 + j == len(patterns) - 1:
-            return [True, smudges]
+        if i - j == 0 or i + 1 + j == len(l) - 1:
+            return [True, s]
 
-def checkBlock(list):
-    for i, el in enumerate(list):
-        smudges = 0
-        if i + 1 >= len(list) or (not el == list[i + 1] and not validSmudge(el, list[i + 1])):
+def isValid(l):
+    for i, el in enumerate(l):
+        s = 0
+        if i + 1 >= len(l) or (not el == l[i + 1] and not isSmudge(el, l[i + 1])):
             continue
 
-        smudges += 1 if validSmudge(el, list[i + 1]) and (not i == 0 and not i == len(list) - 2) else 0
-        if (fnResult := processPatterns(list, i, smudges)) and fnResult[1] == 1:
+        s += 1 if isSmudge(el, l[i + 1]) and (not i == 0 and not i == len(l) - 2) else 0
+        if (fnR := process(l, i, s)) and fnR[1] == 1:
             return i + 1
 
     return -1
 
-def getResultFromPatterns(patterns):
-    if not (position := checkBlock(patterns)) == -1:
-        return position * 100
-    if not (position := checkBlock([list(x) for x in zip(*patterns)])) == -1:
-        return position
-    return 0
+def summarize(l):
+    return i * 100 if not (i := isValid(l)) == -1 else isValid([list(x) for x in zip(*l)])
 
-patterns, result = [], 0
+list, result = [], 0
 for line in getLinesFromFile("day-13"):
     if not line == "\n":
-        patterns.append(re.compile(r'[#\.]').findall(line)); continue
-    result, patterns = result + getResultFromPatterns(patterns), []
+        list.append(re.compile(r'[#\.]').findall(line)); continue
+    result, list = result + summarize(list), []
 
-result += getResultFromPatterns(patterns)
-
-print("Result", result)
+print(f'\n---------------- Result: {result + summarize(list)} ----------------\n')
 
 # Answer: 44615

e878eca0f60421575f5374fcd3fe51472d92b0bb
Amin Khayam <aminkhayam@fattureincloud.it>
2023-12-16 15:30:55 +0100

    things for day-02

--- day-02/solution.py
+++ day-02/solution.py
@@ -1,39 +1,20 @@
-import sys
-import re
+import sys, re
 sys.path.append('/Users/aminkhayam/GIT/private/advent-of-code-2023/utils')
 
 from utils import getLinesFromFile, getNumbers
 
-maxRed = 12
-maxGreen = 13
-maxBlue = 14
-
 result = 0
+colors = [['red', 12], ['green', 13], ['blue', 14]]
 
+def isExceeded(line, max, reg):
+    for item in reg.findall(line):
+        if(int(getNumbers(item)[0]) > max):
+            return True
+    return False
 
-
-for line in getLinesFromFile("day-02"):
-    exceeded = False
-
-    for red in re.compile(r'\d+ red').findall(line):
-        if(int(getNumbers(red)[0]) > maxRed):
-            exceeded = True
-            break
-
-    if (exceeded == False):
-        for green in re.compile(r'\d+ green').findall(line):
-            if(int(getNumbers(green)[0]) > maxGreen):
-                exceeded = True
-                break
-
-    if (exceeded == False):
-        for blue in re.compile(r'\d+ blue').findall(line):
-            if(int(getNumbers(blue)[0]) > maxBlue):
-                exceeded = True
-                break
-
-    if(exceeded == False):
-        result += int(getNumbers(re.compile(r'Game \d+').findall(line)[0])[0])
+for l in getLinesFromFile("day-02"):
+    if not [True for c in colors if isExceeded(l, c[1], re.compile(r'\d+ %s' % c[0]))]:
+        result += int(getNumbers(re.compile(r'Game \d+').findall(l)[0])[0])
 
 print(result)
 
